79378e34cff978164fa3c334a802630b7d94be91
VeronikaLapa <Lapshina.nika@yandex.ru>
2019-09-27 18:22:25 +0300

    base version

---
+++ src/main/java/com/example/demoname/DemonameApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonameApplication.class, args);
+	}
+
+}
