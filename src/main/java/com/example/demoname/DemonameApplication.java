f0781cde8c1d062efb9895f986690b9d60c1a775
Daria <darijapond@gmail.com>
2019-09-28 16:42:58 +0300

    properties

--- src/main/java/com/example/demoname/DemonameApplication.java
+++ src/main/java/com/example/demoname/DemonameApplication.java
@@ -1,5 +1,7 @@
 package com.example.demoname;
 
+import com.example.demoname.external.VkProfileParser;
+import com.example.demoname.external.VkWebRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class DemonameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemonameApplication.class, args);
+
+		VkWebRequest Request = new VkWebRequest();
+
+		VkProfileParser Parser = new VkProfileParser(Request.getPostParsedJson("ranyarwen.sinderama", 1), "ranyarwen.sinderama");
+		Parser.debug();
 	}
 
 }
