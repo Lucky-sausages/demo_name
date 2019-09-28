58ec186b345798aa5e10bfc39b129bef3ff82a78
Артем Крылов <kav128@outlook.com>
2019-09-28 16:38:42 +0300

    Some optimization

--- src/main/java/com/example/demoname/external/InstagramProfileParser.java
+++ src/main/java/com/example/demoname/external/InstagramProfileParser.java
@@ -1,12 +1,12 @@
 package com.example.demoname.external;
 
+import com.example.demoname.dto.MediaDTO;
+import com.example.demoname.dto.PostDTO;
 import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.PathNotFoundException;
 import net.minidev.json.JSONArray;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class InstagramProfileParser
 {
@@ -30,12 +30,22 @@ public class InstagramProfileParser
             queryMap.put("user.id", "$.graphql.user.id");
             queryMap.put("has_next_page", "$.graphql.user.edge_owner_to_timeline_media.page_info.has_next_page");
             queryMap.put("end_cursor", "$.graphql.user.edge_owner_to_timeline_media.page_info.end_cursor");
+
+            queryMap.put("typename", "$.graphql.user.edge_owner_to_timeline_media.edges[%d].node.__typename");
+            queryMap.put("caption", "$.graphql.user.edge_owner_to_timeline_media.edges[%d].node.edge_media_to_caption.edges[0].node.text");
+            queryMap.put("display_url", "$.graphql.user.edge_owner_to_timeline_media.edges[%d].node.display_url");
+            queryMap.put("timestamp", "$.graphql.user.edge_owner_to_timeline_media.edges[%d].node.taken_at_timestamp");
         }
         else
         {
             queryMap.put("shortcode", "$.data.user.edge_owner_to_timeline_media.edges.[*].node.shortcode");
             queryMap.put("has_next_page", "$.data.user.edge_owner_to_timeline_media.page_info.has_next_page");
             queryMap.put("end_cursor", "$.data.user.edge_owner_to_timeline_media.page_info.end_cursor");
+
+            queryMap.put("typename", "$.data.user.edge_owner_to_timeline_media.edges[%d].node.__typename");
+            queryMap.put("caption", "$.data.user.edge_owner_to_timeline_media.edges[%d].node.edge_media_to_caption.edges[0].node.text");
+            queryMap.put("display_url", "$.data.user.edge_owner_to_timeline_media.edges[%d].node.display_url");
+            queryMap.put("timestamp", "$.data.user.edge_owner_to_timeline_media.edges[%d].node.taken_at_timestamp");
         }
     }
 
@@ -45,6 +55,46 @@ public class InstagramProfileParser
         JSONArray array = parsedData.read(queryMap.get("shortcode"));
         for (Object item : array)
             outputList.add((String)item);
+        return outputList;
+    }
+
+    public List<PostDTO> getPosts()
+    {
+        List<PostDTO> outputList = new ArrayList<>();
+        List<String> shortCodes = getShortCodes();
+        int count = shortCodes.size();
+        for (int i = 0; i < count; i++)
+        {
+            String typeName = parsedData.read(String.format(queryMap.get("typename"), i));
+            PostDTO post;
+            if (Objects.equals(typeName, "GraphImage"))
+            {
+                post = new PostDTO();
+                String link = String.format("https://instagram.com/p/%s/", shortCodes.get(i));
+                String text;
+                try
+                {
+                    text = parsedData.read(String.format(queryMap.get("caption"), i));
+                }
+                catch (PathNotFoundException e)
+                {
+                    text = null;
+                }
+                Date date = new Date((Integer) parsedData.read(String.format(queryMap.get("timestamp"), i)));
+                String displayUrl = parsedData.read(String.format(queryMap.get("display_url"), i));
+                MediaDTO media = new MediaDTO();
+                media.setLink(displayUrl);
+                post.setLink(link);
+                post.setText(text);
+                post.setDate(date);
+                List<MediaDTO> mediaList = new ArrayList<>();
+                mediaList.add(media);
+                post.setMedia(mediaList);
+            }
+            else
+                post = InstagramManager.getPostByShortCode(shortCodes.get(i));
+            outputList.add(post);
+        }
         return outputList;
     }
 
