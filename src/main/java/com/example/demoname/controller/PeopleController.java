a5dc4403e334ab2a11ee36e5d48da80b59342dbf
Артем Крылов <kav128@outlook.com>
2019-09-28 19:16:58 +0300

    @CrossOrigin annotation

--- src/main/java/com/example/demoname/controller/PeopleController.java
+++ src/main/java/com/example/demoname/controller/PeopleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/people")
 public class PeopleController extends ApiController{
     private PeopleService peopleService;
