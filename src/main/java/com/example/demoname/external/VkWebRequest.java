d1e11ca605e872e5fbad7a42bc50047960919b95
Daria <darijapond@gmail.com>
2019-09-28 16:14:58 +0300

    vk

---
+++ src/main/java/com/example/demoname/external/VkWebRequest.java
@@ -0,0 +1,34 @@
+package com.example.demoname.external;
+
+import com.jayway.jsonpath.DocumentContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VkWebRequest extends WebRequest{
+
+    @Value("${vk.key}")
+    private String secret;
+
+    private static VkWebRequest instance;
+    private Integer count = 12;
+    private String filter = "owner";
+
+
+    static VkWebRequest getInstance()
+    {
+        if (instance == null)
+            instance = new VkWebRequest();
+        return instance;
+    }
+
+
+    public DocumentContext getPostParsedJson(String domain, Integer offset)
+    {
+        secret = "7e6f03dd7e6f03dd7e6f03ddd77e021f8377e6f7e6f03dd23e153cd52192e2ce2913c04";
+        String url = String.format("https://api.vk.com/method/wall.get?domain=" + domain + "&offset="+ offset + "&count=" + count + "&filter=" + filter +"&access_token=" + secret + "&v=5.101");
+        return getParsedJson(url);
+    }
+
+
+}
