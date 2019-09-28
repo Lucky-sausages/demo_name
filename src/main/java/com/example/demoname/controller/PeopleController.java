0c912ad6882a72bffdba3a419fa47438635cf889
VeronikaLapa <Lapshina.nika@yandex.ru>
2019-09-29 01:10:52 +0300

    Cors fixed

--- src/main/java/com/example/demoname/controller/PeopleController.java
+++ src/main/java/com/example/demoname/controller/PeopleController.java
@@ -6,14 +6,12 @@ import com.example.demoname.exception.*;
 import com.example.demoname.service.PeopleService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/people")
 public class PeopleController extends ApiController{
     private PeopleService peopleService;
